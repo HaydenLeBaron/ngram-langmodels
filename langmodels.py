ba9454cbe8bf09c6a16cc005816aaef5b50ddea7
Hayden LeBaron <hlebaron98@gmail.com>
2021-09-29 06:16:04 -0600

    Parse training file into tokens

--- langmodels.py
+++ langmodels.py
@@ -1,6 +1,25 @@
+import sys
 
+"""
+The main entry point of this program.
+"""
 def main():
-    print('hello world')
+    training_file_path = sys.argv[1]
+    cli_flag = sys.argv[2] # Expects "-test"
+    test_file_path = sys.argv[3]
+
+    training_file_tokens = []
+    with open(training_file_path) as training_file:
+        for line in training_file:
+            training_file_tokens.append(line.split())
+
+    print(training_file_tokens)
+
+
+
+
+
+
 
 
 
