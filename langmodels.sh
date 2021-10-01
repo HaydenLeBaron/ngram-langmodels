18e1cbff5ddd8fcab5c9b9b1232a064d2a964b4f
Hayden LeBaron <hlebaron98@gmail.com>
2021-10-01 16:48:40 -0600

    Edit script

--- langmodels.sh
+++ langmodels.sh
@@ -1,1 +1,5 @@
-python3 langmodels.py training.txt -test test.txt > langmodels-output.txt
+rm langmodels-output.txt
+echo "TRAIN/TEST 1:\n" >> langmodels-output.txt
+python3 langmodels.py train1.txt -test test1.txt >> langmodels-output.txt
+echo "TRAIN/TEST 2:\n" >> langmodels-output.txt
+python3 langmodels.py train2.txt -test test2.txt >> langmodels-output.txt
